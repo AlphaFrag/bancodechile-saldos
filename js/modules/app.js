a03d64a333efab6cabffca39ab3d3a695bb8c6a8
Fernando Silva <fdograph@gmail.com>
2014-11-17 09:00:00 -0300

    post entrega preliminar

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,36 +1,55 @@
-var saldosApp = angular.module('saldosApp', ['ngRoute','saldosController']);
+var saldosApp = angular.module('saldosApp', ['ngRoute', 'saldosController']);
 
 saldosApp.config(['$routeProvider', '$controllerProvider', '$locationProvider',
-  function($routeProvider, $controllerProvider, $locationProvider) {
-    $routeProvider.
-      when('/inicio', {
-        templateUrl: 'partials/inicio.html',
-        controller: 'mainController as main'
-      }).
-      when('/olvido', {
-        templateUrl: 'partials/olvido-clave.html'
-      }).
-      when('/corriente', {
-        templateUrl: 'partials/corriente.html',
-        controller: 'mainController as main'
-      }).
-      when('/tarjeta', {
-        templateUrl: 'partials/tarjeta.html',
-        controller: 'mainController as main'
-      }).
-      when('/document', {
-        templateUrl: 'partials/documento.html',
-        controller: 'mainController as main'
-      }).
-      when('/config', {
-        templateUrl: 'partials/configuracion.html',
-        controller: 'mainController as main'
-      }).
-      otherwise({
-        templateUrl: 'templates/acceso.html'
-      });
+    function($routeProvider, $controllerProvider, $locationProvider) {
+        $routeProvider.
+        when('/inicio', {
+            templateUrl: 'partials/inicio.html',
+            controller: 'mainController as main'
+        }).
+        when('/olvido', {
+            templateUrl: 'partials/olvido-clave.html'
+        }).
+        when('/corriente', {
+            templateUrl: 'partials/corriente.html',
+            controller: 'mainController as main'
+        }).
+        when('/tarjeta', {
+            templateUrl: 'partials/tarjeta.html',
+            controller: 'mainController as main'
+        }).
+        when('/document', {
+            templateUrl: 'partials/documento.html',
+            controller: 'mainController as main'
+        }).
+        when('/config', {
+            templateUrl: 'partials/configuracion.html',
+            controller: 'mainController as main'
+        }).
+        when('/cambiar-clave-seguridad', {
+            templateUrl: 'partials/cambiar-clave-seguridad.html',
+            controller: 'mainController as main'
+        }).
+        when('/mensajes', {
+            templateUrl: 'partials/mensajes.html',
+            controller: 'mainController as main'
+        }).
+        when('/mis-aplicaciones', {
+            templateUrl: 'partials/mis-aplicaciones.html',
+            controller: 'mainController as main'
+        }).
+        when('/ayuda', {
+            templateUrl: 'partials/ayuda.html',
+            controller: 'mainController as main'
+        }).
+        when('/enviar-twitter', {
+            templateUrl: 'partials/enviar-twitter.html',
+            controller: 'mainController as main'
+        }).
+        otherwise({
+            templateUrl: 'templates/acceso.html'
+        });
 
-      $locationProvider.html5Mode(true);
-
-
-  }]);
+        $locationProvider.html5Mode(true);
+    }
+]);
